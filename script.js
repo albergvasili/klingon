18cc3a3b2b489d1d8d8ed1850b94624d8829b741
Vasili <albergvasili@yandex.com>
2024-05-01 18:43:28 -0400

    Add level1 words to dictionary

--- script.js
+++ script.js
@@ -1,18 +1,123 @@
 //import dictionary from './dictionary.js';
-let dictionary = { 
-  level1: { 
-    loD: { 
-      klingon: "loD", 
-      nype: "noun", 
-      english: "man"
-    }, 
-    be_: { 
-      klingon: "be'", 
-      type: "noun", 
-      english: "woman", 
-    }, 
-  }, 
-}; 
+let dictionary = {
+  level1: {
+    loD: {
+      klingon: "loD",
+      nype: "noun",
+      english: "man",
+    },
+    be_: {
+      klingon: "be'",
+      type: "noun",
+      english: "woman",
+    },
+    jup: {
+      klingon: "jup",
+      type: "noun",
+      english: "friend",
+    },
+    jagh: {
+      klingon: "jagh",
+      type: "noun",
+      english: "ennemy",
+    },
+    puq: {
+      klingon: "puq",
+      type: "noun",
+      english: "child",
+    },
+    Saj: {
+      klingon: "Saj",
+      type: "noun",
+      english: "pet",
+    },
+    HoD: {
+      klingon: "HoD",
+      type: "noun",
+      english: "captain",
+    },
+    yaS: {
+      klingon: "yaS",
+      type: "noun",
+      english: "officer",
+    },
+    nov: {
+      klingon: "nov",
+      type: "noun",
+      english: "alien",
+    },
+    yiH: {
+      klingon: "yiH",
+      type: "noun",
+      english: "tribble",
+    },
+    Qel: {
+      klingon: "Qel",
+      type: "noun",
+      english: "doctor",
+    },
+    SID: {
+      klingon: "SID",
+      type: "noun",
+      english: "patient",
+    },
+    qorDu_: {
+      klingon: "qorDu'",
+      type: "noun",
+      english: "family",
+    },
+    ghu: {
+      klingon: "ghu",
+      type: "noun",
+      english: "baby",
+    },
+    tlhIngan: {
+      klingon: "tlhIngan",
+      type: "noun",
+      english: "klingon",
+    },
+    jaH: {
+      klingon: "jaH",
+      type: "verb",
+      english: "goes",
+    },
+    ghoS: {
+      klingon: "ghoS",
+      type: "verb",
+      english: "comes",
+    },
+    Sop: {
+      klingon: "Sop",
+      type: "verb"
+      english: "eats"
+    },
+    tlhutlh: {
+      klingon: "tlhutlh",
+      type: "verb",
+      english: "drinks",
+    },
+    jatlh: {
+      klingon: "jathl",
+      type: "verb",
+      english: "speaks",
+    },
+    yaj: {
+      klingon: "yaj",
+      type: "verb",
+      english: "understands",
+    },
+    laD: {
+      klingon: "laD",
+      type: "verb",
+      english: "reads",
+    },
+    ghItlh: {
+      klingon: "ghItlh",
+      type: "verb",
+      english: "writes",
+    },
+  },
+};
  
 function displayWords(level) {
   let dict = dictionary[level];
