8878f23210670493b924c2e0b99aaca543f51e26
Vasili <albergvasili@yandex.com>
2024-05-01 18:28:32 -0400

    Refactor logic to display words by level
    -Create displayWords function

--- script.js
+++ script.js
@@ -14,38 +14,39 @@ let dictionary = {
   }, 
 }; 
  
-function wordSelect(level) {
+function displayWords(level) {
   let dict = dictionary[level];
   let words = Object.keys(dict);
+  let lesson = document.getElementById("lesson");
 
-  console.log(words);
+  for (let word in dict) {
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    let button = document.createElement("input");
 
-}
+    label.textContent = dict[word].klingon;
+
+    input.setAttribute("type", "text");
+    input.setAttribute("id", dict[word].klingon);
+
+    button.setAttribute("type", "submit");
+    button.addEventListener("click", () => {
+      console.log(typeof input.value, typeof dict[word].english);
+      checkAnswer(input, dict[word].english);
+    });
+
+    lesson.append(label, input, button);
+  };
+};
 
 let testFunction = document.getElementById("start");
-testFunction.addEventListener("click", () => wordSelect("level1")); //testing function
+testFunction.addEventListener("click", () => displayWords("level1")); //testing function
 
-let lesson = document.getElementById("lesson");
-
-let form = document.createElement("form");
-let label = document.createElement("label");
-let input = document.createElement("input");
-let button = document.createElement("input");
-label.textContent = dictionary.level1.loD.klingon;
-input.setAttribute("type", "text");
-input.setAttribute("id", "loD");
-button.setAttribute("type", "submit");
-button.addEventListener("click", test);
-
-
-  function test() {
-    if (input.value == dictionary.level1.loD.english) {
-      console.log("oui");
-    } else {
-      console.log("non");
-};
+function checkAnswer(input, answer) {
+  if (input.value == answer) {
+    console.log("oui");
+  } else {
+    console.log("non");
+  };
 };
 
-
-
-lesson.append(label, input, button);
